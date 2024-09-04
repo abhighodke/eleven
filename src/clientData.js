0755d0bc415bf361d7d201a00b0c4437923f7292
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:38:10 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "JR",
-    heroTagline: "Jacob Roll Realty",
-    agentName: "Jacob Roll",
+    headerInitials: "SG",
+    heroTagline: "Sharon Gilbert Realty",
+    agentName: "Sharon Gilbert",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Jacob Roll was a game-changer for us. His deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Jacob Roll provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Jacob Roll were evident throughout our home-buying journey. His ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Jacob Roll and His team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Jacob Roll is the one. His dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Sharon Gilbert was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Sharon Gilbert provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Sharon Gilbert were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Sharon Gilbert and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Sharon Gilbert is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "Soldwithjroll@gmail.com",
-    address: "1400 Rayburn Avenue\nGuntersville, AL 35976",
-    phone: "256-571-5627",
-    footerDescription: `Jacob Roll Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "sharon.hall@kw.com",
+    address: "1805 Hillyer Robinson PKWAY \nAnniston, AL 36207",
+    phone: "256-282-8522",
+    footerDescription: `Sharon Gilbert Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/rollwithjacob",
+        facebook: "https://www.facebook.com/sharon.turner.10/",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "Soldwithjroll@gmail.com",
-        phone: "256-571-5627",
+        email: "sharon.hall@kw.com",
+        phone: "256-282-8522",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/396715597_10158245627917537_3644436346653421163_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=iWdgd7WWVk0Q7kNvgHKnQGC&_nc_ht=scontent-sjc3-1.xx&oh=00_AYDQ2MYrxLZA00ywJu3RHj8ry2v0MW-C82p1g7doPydl4A&oe=66D7D3FA" // Agent image URL
+    dp: "https://greateralabamamlsimages.fnistools.com/images/uploads/agents/11268441/11268441.jpg" // Agent image URL
 };
 
 export default clientData;
